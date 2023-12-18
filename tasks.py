681faa0fc6b9024b8138de98fe62b3f1e2b2b7f0
Nils Ost <home@nijos.de>
2023-12-18 08:39:58 +0100

    ensure directorys for dev-containers do exist

--- tasks.py
+++ tasks.py
@@ -6,10 +6,12 @@ def start_development(c):
     r = c.run("sudo docker ps -f name=dev-mongo", hide=True)
     if 'dev-mongo' not in r.stdout:
         print("Starting mongoDB")
+        c.run("sudo mkdir -p /media/ramdisk/mongodb")
         c.run("sudo docker run --name dev-mongo --rm -v /media/ramdisk/mongodb:/data/db -p 27017:27017 -d mongo:4.4")
     r = c.run("sudo docker ps -f name=dev-influx", hide=True)
     if 'dev-influx' not in r.stdout:
         print("Starting influxDB")
+        c.run("sudo mkdir -p /media/ramdisk/influxdb")
         c.run("sudo docker run --name dev-influx --rm -v /media/ramdisk/influxdb:/var/lib/influxdb -p 8086:8086 -d influxdb:1.8-alpine")
     r = c.run("sudo docker ps -f name=dev-mosquitto", hide=True)
     if 'dev-mosquitto' not in r.stdout:
@@ -19,6 +21,7 @@ def start_development(c):
     r = c.run("sudo docker ps -f name=dev-minio", hide=True)
     if 'dev-minio' not in r.stdout:
         print("Starting MinIO")
+        c.run("sudo mkdir -p /media/ramdisk/minio")
         c.run('sudo docker run --name dev-minio --rm -v /media/ramdisk/minio:/data -p 9000:9000 --env MINIO_ROOT_USER="brickserver" --env MINIO_ROOT_PASSWORD="password" -d minio/minio:RELEASE.2021-06-17T00-10-46Z server /data')
     c.run("python install/wait_for_influxdb.py")
     c.run("python install/wait_for_mosquitto.py")
