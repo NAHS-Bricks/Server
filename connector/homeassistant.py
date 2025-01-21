7546d8870653de99fb7956483b3377991d7083e7
Nils Ost <home@nijos.de>
2025-01-21 08:58:39 +0100

    Some fixes encountered during testing

--- connector/homeassistant.py
+++ connector/homeassistant.py
@@ -3,6 +3,7 @@ from connector.mqtt import _publish_async, bat_level_send, temp_send, humid_send
 from helpers.current_version import current_brickserver_version as bs_version
 from helpers.shared import config
 from multiprocessing import Process
+import json
 
 
 async_listener = None
@@ -10,12 +11,22 @@ discovery_prefix = config['mqtt']['ha_discovery_prefix']
 
 # brick_type to model (mdl) map
 type_mdl_map = {
-    1: 'TempBrick'
+    1: 'TempBrick',
+    2: 'LatchBrick',
+    3: 'SignalBrick',
+    4: 'RackBrick',
+    5: 'RHTBrick',
+    6: 'HeatBrick'
 }
 
 # brick_type to hw_version (hw) map
 type_hw_map = {
-    1: '1.0'
+    1: '1.0',
+    2: '1.0',
+    3: '1.0',
+    4: '1.0',
+    5: '1.0',
+    6: '1.0'
 }
 
 
@@ -25,7 +36,7 @@ def __cmp_bat_percent(brick_id):
         'unique_id': f'brick{brick_id}batvoltage',
         'name': 'Battery',
         'device_class': 'battery',
-        'state_topic': '~/bat/voltage',
+        'state_topic': f'brick/{brick_id}/bat/voltage',
         'value_template': '{{ 100 / 0.8 * (min([value, 4.2]) - 3.4) | round(0) }}'
     }
     return result
@@ -37,7 +48,7 @@ def __cmp_bat_prediction(brick_id):
         'unique_id': f'brick{brick_id}batprediction',
         'name': 'Battery Prediction',
         'device_class': 'duration',
-        'state_topic': '~/bat/prediction',
+        'state_topic': f'brick/{brick_id}/bat/prediction',
         'unit_of_measurement': 'd',
         'suggested_display_precision': 0,
         'icon': 'mdi:battery-clock-outline'
@@ -51,7 +62,8 @@ def __cmp_temp(brick_id, sensor):
         'platform': 'sensor',
         'unique_id': f'brick{brick_id}temp{sensor_id}',
         'device_class': 'temperature',
-        'state_topic': f'~/temp/{sensor_id}'
+        'state_topic': f'brick/{brick_id}/temp/{sensor_id}',
+        'name': f'Temperature {sensor_id}'
     }
     if sensor['desc'] is not None and not sensor['desc'] == '':
         result['name'] = sensor['desc']
@@ -64,7 +76,8 @@ def __cmp_humid(brick_id, sensor):
         'platform': 'sensor',
         'unique_id': f'brick{brick_id}humid{sensor_id}',
         'device_class': 'humidity',
-        'state_topic': f'~/humid/{sensor_id}'
+        'state_topic': f'brick/{brick_id}/humid/{sensor_id}',
+        'name': f'Humidity {sensor_id}'
     }
     if sensor['desc'] is not None and not sensor['desc'] == '':
         result['name'] = sensor['desc']
@@ -73,12 +86,14 @@ def __cmp_humid(brick_id, sensor):
 
 def __cmp_latch(brick_id, sensor):
     sensor_id = sensor['_id']
+    _, latch_id = sensor_id.split('_')
     result = {
         'platform': 'sensor',
         'unique_id': f'brick{brick_id}latch{sensor_id}',
         'device_class': 'enum',
         'options': sensor['states_desc'],
-        'state_topic': f'~/latch/{sensor_id}'
+        'state_topic': f'brick/{brick_id}/latch/{sensor_id}',
+        'name': f'Latch {latch_id}'
     }
     if sensor['desc'] is not None and not sensor['desc'] == '':
         result['name'] = sensor['desc']
@@ -94,8 +109,7 @@ def send_device_config(brick=None, brick_id=None):
         return
     brick_id = brick['_id']
     payload = dict()
-    payload['~'] = f'/brick/{brick_id}'
-    payload['device'] = {'ids': brick_id, 'mf': 'NiJOs', 'mdl': type_mdl_map[brick['type']], 'hw': type_hw_map[brick['type']]}
+    payload['device'] = {'ids': brick_id, 'name': brick_id, 'mf': 'NiJOs', 'mdl': type_mdl_map[brick['type']], 'hw': type_hw_map[brick['type']]}
     if brick['desc'] is not None and not brick['desc'] == '':
         payload['device']['name'] = brick['desc']
     payload['origin'] = {'name': 'BrickServer', 'sw': bs_version, 'url': 'https://bricks.nijos.de'}
@@ -119,7 +133,7 @@ def send_device_config(brick=None, brick_id=None):
                 latch = latch_get(brick_id, latch_id)
                 if 'mqtt' not in latch['disables']:
                     payload['components'][f'latch{latch_id}'] = __cmp_latch(brick_id, latch)
-    _publish_async(topic=f'{discovery_prefix}/device/{brick_id}/config', payload=payload)
+    _publish_async(topic=f'{discovery_prefix}/device/{brick_id}/config', payload=json.dumps(payload))
 
 
 def send_sensor_states(brick=None, brick_id=None):
@@ -160,6 +174,7 @@ def start_async_listener():  # pragma: no cover
 
         def _on_message(client, userdata, message):
             if message.payload.decode('utf-8') == config['mqtt']['ha_birth_msg']:
+                print('Received birth message from Home Assistant. Transmitting Brick-Configs ans latest Seansor-States')
                 for brick in brick_all():
                     send_device_config(brick=brick)
                 for brick in brick_all():
